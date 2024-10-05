4eb5aab4dd1cd9c6d985e3519708e832dcecb9d4
takuto1127 <tiharu717@gmail.com>
2024-10-05 10:34:50 +0900

    firestoreのオブジェクトのパースやる前まで

---
+++ lib/model/picture_model.freezed.dart
@@ -0,0 +1,150 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'picture_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$PictureModel {
+  String get name => throw _privateConstructorUsedError;
+  String get url => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $PictureModelCopyWith<PictureModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $PictureModelCopyWith<$Res> {
+  factory $PictureModelCopyWith(
+          PictureModel value, $Res Function(PictureModel) then) =
+      _$PictureModelCopyWithImpl<$Res, PictureModel>;
+  @useResult
+  $Res call({String name, String url});
+}
+
+/// @nodoc
+class _$PictureModelCopyWithImpl<$Res, $Val extends PictureModel>
+    implements $PictureModelCopyWith<$Res> {
+  _$PictureModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? name = null,
+    Object? url = null,
+  }) {
+    return _then(_value.copyWith(
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      url: null == url
+          ? _value.url
+          : url // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$PictureModelImplCopyWith<$Res>
+    implements $PictureModelCopyWith<$Res> {
+  factory _$$PictureModelImplCopyWith(
+          _$PictureModelImpl value, $Res Function(_$PictureModelImpl) then) =
+      __$$PictureModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({String name, String url});
+}
+
+/// @nodoc
+class __$$PictureModelImplCopyWithImpl<$Res>
+    extends _$PictureModelCopyWithImpl<$Res, _$PictureModelImpl>
+    implements _$$PictureModelImplCopyWith<$Res> {
+  __$$PictureModelImplCopyWithImpl(
+      _$PictureModelImpl _value, $Res Function(_$PictureModelImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? name = null,
+    Object? url = null,
+  }) {
+    return _then(_$PictureModelImpl(
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      url: null == url
+          ? _value.url
+          : url // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$PictureModelImpl implements _PictureModel {
+  const _$PictureModelImpl({required this.name, required this.url});
+
+  @override
+  final String name;
+  @override
+  final String url;
+
+  @override
+  String toString() {
+    return 'PictureModel(name: $name, url: $url)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$PictureModelImpl &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.url, url) || other.url == url));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, name, url);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$PictureModelImplCopyWith<_$PictureModelImpl> get copyWith =>
+      __$$PictureModelImplCopyWithImpl<_$PictureModelImpl>(this, _$identity);
+}
+
+abstract class _PictureModel implements PictureModel {
+  const factory _PictureModel(
+      {required final String name,
+      required final String url}) = _$PictureModelImpl;
+
+  @override
+  String get name;
+  @override
+  String get url;
+  @override
+  @JsonKey(ignore: true)
+  _$$PictureModelImplCopyWith<_$PictureModelImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
--- lib/testt/firebase_test.dart
+++ lib/testt/firebase_test.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:app/model/picture_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -9,12 +10,43 @@ Future<void> testFirebase() async {
   await ref.get().then((QuerySnapshot<Object?> snapshot) async {
     for (final docSnapShot in snapshot.docs) {
       debugPrint(docSnapShot["name"]);
-      List<dynamic> list = docSnapShot["waypoints"];
-      list.forEach((element) async {
-        await element.get().then((DocumentSnapshot value) {
-          debugPrint(value["name"]);
-          debugPrint(((value["geopoint"]) as GeoPoint).latitude.toString());
+      List<dynamic> waypoints = docSnapShot["waypoints"];
+
+      waypoints.forEach((element) async {
+        debugPrint(element.toString());
+        DocumentReference<Map<String, dynamic>> docRef =
+            element as DocumentReference<Map<String, dynamic>>;
+        docRef.get().then((DocumentSnapshot<Map<String, dynamic>> value) {
+          GeoPoint geo = value["geopoint"] as GeoPoint;
+          debugPrint(geo.latitude.toString());
+          List<dynamic> pictures = value["pictures"];
+          pictures.forEach((value) {
+            DocumentReference<Map<String, dynamic>> pictureRef =
+                value as DocumentReference<Map<String, dynamic>>;
+            pictureRef
+                .get()
+                .then((DocumentSnapshot<Map<String, dynamic>> value) {
+              PictureModel model = PictureModel.fromFirestore(value);
+              debugPrint(model.name);
+            });
+          });
         });
+        //GeoPoint geo = (docSnapShot["geopoint"] as GeoPoint);
+        //debugPrint(geo.latitude.toString());
+        //debugPrint("waypoints");
+        //List<dynamic> pictures = element["pictures"];
+        //pictures.forEach((value) {
+        //  debugPrint("aiueo");
+        //  debugPrint(value);
+        //  //PictureModel model = PictureModel.fromFirestore(value);
+        //  //debugPrint(model.name);
+        //});
+
+        //await element.get().then((DocumentSnapshot value) {
+        //  debugPrint("aiueo");
+        //  PictureModel model = PictureModel.fromFirestore(value);
+        //  debugPrint(model.name);
+        //});
       });
 //      for (DocumentReference<Map<String, dynamic>> element in list) {
 //        await element
