35345e8e00835432b84c7b9163b153eb4bbb8f0d
1002masaki <1002masaki@users.noreply.github.com>
2024-10-11 06:52:27 +0000

    bot: run formatterr
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- lib/title.dart
+++ lib/title.dart
@@ -5,26 +5,23 @@ class TitleScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-            const Text('Application',
-                style: TextStyle(
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
-                )),
-            ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(250, 90),
-                    backgroundColor: Color.fromARGB(255, 62, 209, 235),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(0))),
-                child: const Text(
-                  'クリックでスタート',
-                  style: TextStyle(fontSize: 25, color: Colors.white),
-                )),
-          ])),
+          child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
+        const Text('Application',
+            style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+            )),
+        ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+                minimumSize: const Size(250, 90),
+                backgroundColor: Color.fromARGB(255, 62, 209, 235),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0))),
+            child: const Text(
+              'クリックでスタート',
+              style: TextStyle(fontSize: 25, color: Colors.white),
+            )),
+      ])),
     );
   }
 }
